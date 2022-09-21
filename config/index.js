bdb3a7fb95ef483c70bffec3b65eab22dd9a3989
desirepaths <31194894+desirepaths@users.noreply.github.com>
2022-09-21 14:55:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "2913704c08a82aa54489d10da7dd1524",
+  APP_SECRET: "6174b1734004bdfc4b77383274084f0f",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
